650cb37c00741b9934ca7da6b44a383d4af8a1f0
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-03-07 15:57:29 +0500

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./Header.scss";
 
 const Header = () => {
-  return <div className="header">Фортуна</div>;
+  return (
+    <div className="header">
+      <span className="title">Фортуна</span>
+      <span className="material-icons-outlined">account_circle</span>
+    </div>
+  );
 };
 
 export default Header;
